1ecb8a1c1b99a0cb830e8b00256633dcaae78789
Italo Lelis <italohdc@gmail.com>
2018-04-02 14:51:16 -0300

    Main function

---
+++ Simulador.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Environment.h"
+
+
+int main(int agrc,char **argv){
+    Environment Simulator = Environment(argv);
+    Simulator.UpdateState();
+}
