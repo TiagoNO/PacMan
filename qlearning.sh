5f0d431a94dc623a07228f1fd95503e5c313aff0
Ykaro237 <tiagonegri237@gmail.com>
2018-04-03 11:27:04 -0300

    Now avoids compilation all the time by verifying if was already made

--- qlearning.sh
+++ qlearning.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
 echo "Map: $1"
 echo "Learning rate: $2"
 echo "Discount rate: $3"
 echo "Iterations: $4"
-g++ -o qlearning Simulador.c Environment.c Environment.h DataReader.c DataReader.h Agent.c Agent.h
+FILE=qlearning
+DIR="${PWD}"
+if test -f "$DIR/$FILE";
+then
+    #Programa já compilado, apenas precisa ser executado
+    echo "Already was built, lets roll!"
+else
+    #Programa não foi compilado, vamos compila-lo
+    echo "Program wasn't built, get the bricks!"
+    chmod +x Compilar.sh
+    ./Compilar.sh
+    echo "Finished compilation, lets get started!"
+fi
 ./qlearning $1 $2 $3 $4
