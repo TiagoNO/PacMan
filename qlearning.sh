15de353b95c911e04e1b9d737e950dbc26fcc897
Tiago <tiagonegri237@gmail.com>
2018-04-11 08:53:33 -0300

    Now compiles the project just when need

--- qlearning.sh
+++ qlearning.sh
@@ -5,15 +5,16 @@ echo "Discount rate: $3"
 echo "Iterations: $4"
 FILE=qlearning
 DIR="${PWD}"
-#if test -f "$DIR/$FILE";
-#then
-    #Programa já compilado, apenas precisa ser executado
-#    echo "Already was built, lets roll!"
-#else
-    #Programa não foi compilado, vamos compila-lo
-#    echo "Program wasn't built, get the bricks!"
+if test -f "$DIR/$FILE";
+then
+    Programa já compilado, apenas precisa ser executado
+    echo "Already was built, lets roll!"
+else
+    Programa não foi compilado, vamos compila-lo
+    echo "Program wasn't built, get the bricks!"
     chmod +x Compilar.sh
     ./Compilar.sh
-#    echo "Finished compilation, lets get started!"
-#fi
-./qlearning $1 $2 $3 $4
+    echo "Finished compilation, lets get started!"
+fi
+time ./qlearning $1 $2 $3 $4
+sleep 5
