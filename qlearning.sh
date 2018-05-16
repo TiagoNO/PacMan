e6b9ab6e65322778944784153c6988065b31661c
Tiago <tiagonegri237@gmail.com>
2018-05-16 16:12:52 -0300

    now compiles everytime

--- qlearning.sh
+++ qlearning.sh
@@ -5,15 +5,18 @@ echo "Discount rate: $3"
 echo "Iterations: $4"
 FILE=qlearning
 DIR="${PWD}"
-if test -f "$DIR/$FILE";
-then
-    Programa já compilado, apenas precisa ser executado
-    echo "Already was built, lets roll!"
-else
-    Programa não foi compilado, vamos compila-lo
-    echo "Program wasn't built, get the bricks!"
-    chmod +x Compilar.sh
-    ./Compilar.sh
-    echo "Finished compilation, lets get started!"
-fi
-time ./qlearning $1 $2 $3 $4
+#if test -f "$DIR/$FILE";
+#then
+    #Programa já compilado, apenas precisa ser executado
+#    echo "Already was built, lets roll!"
+#else
+ #   #Programa não foi compilado, vamos compila-lo
+#    echo "Program wasn't built, get the bricks!"
+#    chmod +x Compilar.sh
+#    ./Compilar.sh
+#    echo "Finished compilation, lets get started!"
+#fi
+
+g++ -o qlearning Simulador.cpp DataReader.cpp DataReader.h Agent.cpp Agent.h Environment.cpp Environment.h ApproxAgent.h ApproxAgent.cpp Features.h Features.cpp
+chmod +x qlearning
+time ./qlearning $1 $2 $3 $4 $5
