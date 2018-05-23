90b512af2587abeb2dcf8a0c968f1140b34b6b63
Tiago <tiagonegri237@gmail.com>
2018-05-23 14:20:52 -0300

    the Q-value only update in one function

--- ApproxAgent.h
+++ ApproxAgent.h
@@ -19,19 +19,17 @@ class ApproximateAgent:public QLearningAgent{
                 result += this->weights[(*i)]*this->feature->features[(*i)];
             }            
             this->Qvalue[state.first][state.second][action] = result;
+            return this->Qvalue[state.first][state.second][action];
         }
 
         void Update(pair<int,int> state,pair<int,int> nextState,int action,float reward,float NextQValue,float discount,float learning_rate){
-            printf("State:(%i,%i)\nReward:%f\n\n",state.first,state.second,reward);
             float difference = (float) learning_rate*(reward + discount*NextQValue - this->getQvalue(state,action));
-
+            printf("Q[%i][%i][%i] = %f\n",state.first,state.second,action,this->Qvalue[state.first][state.second][action]);
+            fprintf(this->log,"learning_r: %f,reward: %f,discount: %f,NextQValue: %f,Q[state] : %f\n",learning_rate,reward,discount,NextQValue,this->Qvalue[state.first][state.second][action]);
+            fprintf(this->log,"Q[%i][%i][%i] = %f\n",state.first,state.second,action,this->Qvalue[state.first][state.second][action]);
             std::set<string> f = this->feature->getFeaturesKeys();
-            float result = 0;
             for(std::set<string>::const_iterator i = f.begin(); i != f.end(); i++){
                 this->weights[(*i)] += difference*this->feature->features[(*i)];
-                result += this->weights[(*i)]*this->feature->features[(*i)];
             }
-            this->Qvalue[state.first][state.second][action] = result;
-            //printf("%f\n",this->Qvalue[state.first][state.second][action]);
         }
 };
