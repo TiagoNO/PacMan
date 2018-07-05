1826dfd27673df44b3d2f54e6309392b65cb8491
Ykaro237 <tiagonegri237@gmail.com>
2018-07-05 14:54:46 -0300

    an bash program that finds the minimum number of iterations for all values of learning rate and discount rate

---
+++ analyse.sh
@@ -0,0 +1,33 @@
+otmPolicy="pi-star.txt"
+agentPolicy="pi.txt"
+executable="qlearning.sh"
+map="maps/pacmaze-01-tiny.txt"
+outFile="analyse_results.txt"
+
+
+for learningRate in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
+do
+    for discountRate in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
+    do
+        iterations=$((1))
+        echo $learningRate $discountRate
+        while [ 1 ]
+        do
+            ./$executable $map $learningRate $discountRate $iterations 
+            diff $otmPolicy $agentPolicy > /dev/null 2>&1
+            error=$?
+            if [ $error -eq 0 ]
+            then
+                #echo "$otmPolicy and $agentPolicy) are the same file"
+                echo $learningRate $discountRate $iterations "\n"
+            elif [ $error -eq 1 ]
+            then
+                echo "$otmPolicy and $agentPolicy (i=$iterations,l_r=$learningRate,d_r=$discountRate) differ"
+            elif [ $error -eq 2 ]
+            then
+                echo "something went wrong (File ($otmPolicy|$agentPolicy) exists?)"
+            fi
+        iterations=$((iterations + 50))
+        done
+    done
+done
